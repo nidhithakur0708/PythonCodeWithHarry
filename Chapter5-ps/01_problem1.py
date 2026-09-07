801dd55ff8dbf04336460c2a33029dd90f68445a
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:34:28 +0530

    Create 01_problem1.py

---
+++ Chapter5-ps/01_problem1.py
@@ -0,0 +1,10 @@
+#Write a program to create a dict of Hindi words with values as their English meaning. Use dict methods to perform the following operations:
+
+words={
+	"madad":"help",
+	"pyaar":"love",
+	"khushi":"happiness",
+}
+word=input("Enter a Hindi word to get its English meaning: ")
+print(words[word])
+#Accessing value using key
