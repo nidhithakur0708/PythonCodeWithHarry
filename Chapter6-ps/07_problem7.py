cb03484dabeeed2ce9aeb657d46f9319351b2cef
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:43:39 +0530

    Add script to check if post mentions 'Harry'

---
+++ Chapter6-ps/07_problem7.py
@@ -0,0 +1,6 @@
+#WAP to find wheter the given post is talking about "Harry or not"
+post="Hey harry bhai is good!"
+if("harry" in post.lower()):
+	print("Yes, the post is talking about Harry")
+else:
+	print("No, the post is not talking about Harry")
