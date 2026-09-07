8eb29825899f964b5af1c9102d785d922d7808be
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:33:33 +0530

    Create 04_set_methods.py

---
+++ Chapter5/04_set_methods.py
@@ -0,0 +1,29 @@
+#set methods
+s={34,5634,2345,56,343,23,343,1}
+
+#1 .add() method adds an element to the set
+s.add(100)
+print(s)
+
+#2 .clear() method removes all the elements from the set
+#s.clear()
+print(s)
+
+#Properties of set
+'''
+1.sets are unordered
+2.sets are unindexed
+3.There is not way u can change items in the set
+but you can add or remove items from the set
+4.sets are mutable
+5.sets cannot contain duplicate values
+'''
+
+#2.remove() method removes the specified element from the set
+s.remove(34)
+print(s)
+
+#3.pop() method 
+s.pop()
+print(s)
+
