2567bbd225161a9a7dee81e78bc7c66026aa4ed6
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:38:39 +0530

    Create 09_problem9.py

---
+++ Chapter5-ps/09_problem9.py
@@ -0,0 +1,5 @@
+#9.Can you change values inside a list which is inside a set
+s={8,7,12,"Harry",[1,2]}
+
+#no way u can change items in the set ,In fact u cannot have a list inside a set
+#set needs hashable items and list is not hashable
