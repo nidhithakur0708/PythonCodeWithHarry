9b9d4fb2035d4647eb9004432e224d99ffdfa9fd
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:39:40 +0530

    Create 01_conditionals.py

---
+++ Chapter6/01_conditionals.py
@@ -0,0 +1,11 @@
+#Working according to the conditions
+age=int(input("Enter your age: "))
+
+#If else statement
+if(age>18):
+	print("You are eligible to vote.")
+	print("Good for you!")
+else:
+	print("You are not eligible to vote.")
+
+print("End of program")
