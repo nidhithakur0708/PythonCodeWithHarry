236c985374c601a98f598f3a71ea8f159cd6f9f3
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:33:51 +0530

    Create 05_set_unioun.py

---
+++ Chapter5/05_set_unioun.py
@@ -0,0 +1,9 @@
+s1={1,45,6}
+s2={7,8,1,78}
+print(s1.union(s2)) #union of two sets
+
+print(s1.intersection(s2)) #intersection of two sets
+#print(s1.subset())
+
+p={2,34,56,3,4,5}
+print({34,56}.issubset(p)) #issubset() method returns True if all elements of the set are present in the specified set, otherwise it returns False.)
