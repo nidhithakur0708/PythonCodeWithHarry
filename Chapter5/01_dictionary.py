15fd53b325dd010c773a6271d5e866be1dfa5168
Nidhi Thakur <138863268+nidhithakur0708@users.noreply.github.com>
2026-09-07 16:32:17 +0530

    Create 01_dictionary.py for dictionary examples
    Added a Python script demonstrating dictionary usage.

---
+++ Chapter5/01_dictionary.py
@@ -0,0 +1,16 @@
+
+#Dictionary-Key Value Pairs Collection
+d={} #Empty dictionary
+marks={
+"Nidhi": 90,
+"Ram":89,
+"Simran":78
+}
+#print(marks)
+print(marks["Nidhi"]) #Accessing value using key
+##Dictionary is used to look up to the value of a key. It is a collection of key-value pairs. Each key is unique and maps to a specific value.
+
+#Dictionary is mutable
+#Unorderded
+#Indexed
+#Cannot have duplicate values
